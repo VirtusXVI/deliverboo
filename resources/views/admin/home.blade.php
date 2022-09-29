3dd9ecb68e74082a96b5d5fc48c62c24acdb91d7
LorisP96 <lorispalazzo96@gmail.com>
2022-09-29 10:19:54 +0200

    push

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,8 +2,4 @@
 
 @section('content')
     <h1>{{$user->restaurant_name}}</h1>
-    {{-- QUI CI VA TANTA DI QUELLA ROBA CHE MANUEL BESTEMMIERA'
-    ZITTO BADDY --}}
-    <a href="{{route('admin.dishes.index')}}">Vai ai Piatti</a>
-    <a href="{{route('admin.dishes.create')}}">Vai alla Creazione</a>
 @endsection
