e22d3e072eab4b9152616fa1d4295fc1d4ff0c8a
VirtusXVI <manuel.aveta2001@gmail.com>
2022-09-30 14:15:08 +0200

    corrected position title in index

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -5,9 +5,9 @@
 
     <div class="container">
         @if (count($dishes) > 0)
+            <h1>I tuoi piatti:</h1>
             @foreach ($dishes as $dish)
                 @if ($dish->is_visible == 1)
-                    <h1>I tuoi piatti:</h1>
                     <div class="card mb-3">
                         <h3><a class="card-title" href="{{ route('admin.dishes.show', ['dish' => $dish->id]) }}">{{ $dish->name }}</a></h3>
                     </div>
