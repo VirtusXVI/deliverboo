eab57bdc4031bc633c9e66bcc4be62d6f4ce6a32
LorisP96 <lorispalazzo96@gmail.com>
2022-09-30 10:30:37 +0200

    nuovo elimina

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -12,12 +12,24 @@
                 <p class="card-title">{{ $dish->price }}</p>
                 <div class="d-flex">
                     <a href="{{ route('admin.dishes.edit', ['dish'=>$dish->id]) }}" class="btn btn-primary py-1 px-2 mr-2">Modifica</a>
-                    <form action="{{ route('admin.dishes.destroy', $dish->id) }}" method="post">
+                        
+                    <input class="btn btn-danger py-1 px-2" type="submit" value="Elimina Piatto" onClick="document.getElementById('id01').style.display='block'">
+                    <div id="id01" class="modal">
+                        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+                        <form class="modal-content" action="{{ route('admin.dishes.destroy', $dish->id) }} " method="post">
                         @csrf
                         @method('DELETE')
-            
-                        <input class="btn btn-danger py-1 px-2" type="submit" value="Elimina Piatto" onClick="return confirm('Sicuro di voler concellare il piatto?')">
-                    </form>
+                            <div class="container">
+                            <h1>Elimina Piatto</h1>
+                            <p>Sicuro di voler procedere?</p>
+                        
+                            <div class="clearfix">
+                                <button type="button" class="cancelbtn" onClick="document.getElementById('id01').style.display='none'">Annulla</button>
+                                <button type="button submit" class="deletebtn">Elimina</button>
+                            </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
