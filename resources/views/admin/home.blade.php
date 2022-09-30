2faaf750837c1112484fbc140bb3c1105c61492d
NicoloYan <nico.yan@outlook.it>
2022-09-30 15:00:09 +0200

    added container in home.blade.php

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,6 +2,8 @@
 @extends('admin.navlinks')
 
 @section('content')
-    <h1>{{$user->restaurant_name}}</h1>
+    <div class="container">
+        <h1>{{$user->restaurant_name}}</h1>
+    </div>
     {{-- qui stampiamo le informazioni del ristorante (anche perchè non serve a un cazzo) --}}
 @endsection
