ef7184b00e3ea90eedd0f7d5012b398cb306bf13
zhLisha <lisazhou1709@gmail.com>
2022-10-02 15:01:23 +0200

    Footer section added some links, not finished yet

--- resources/views/layouts/frontoffice.blade.php
+++ resources/views/layouts/frontoffice.blade.php
@@ -1,3 +1,17 @@
+<?php 
+    $footerSocial = [
+        [
+            "social" => "Facebook",
+        ],
+        [
+            "social" => "Instagram",
+        ],
+        [
+            "social" => "Twitter",
+        ]
+    ]
+?>
+
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -21,62 +35,106 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Deliveboo
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        {{-- @yield('navlinks') --}}
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
+        <header>
+            <nav class="navbar navbar-expand-md navbar-light shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        Deliveboo
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
+                            {{-- @yield('navlinks') --}}
+                        </ul>
+    
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle text" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->restaurant_name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle text" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->restaurant_name }}
                                     </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+    
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+    
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
 
         <main>
             @yield('content')
         </main>
 
         @include('sweetalert::alert')
+
+        <footer>
+            <div class="footer-content container">
+                <h2>Deliveroo</h2>
+
+                <div class="links">
+                    {{-- Social --}}
+                    <div class="social">
+                        <div class="title">
+                            <h3>Social</h3>
+                        </div>
+                        @foreach ($footerSocial as $list)
+                            <li>
+                                <a href="#"><span> {{$list['social']}}</span></a>
+                            </li>
+                        @endforeach
+                    </div>
+
+                    {{-- Contact --}}
+                    <div class="contacts">
+                        <div class="title">
+                            <h3>Contatti</h3>
+                        </div>
+                        <div class="address">
+                        <Span>Via XXXXXX, 17 - Italy</Span>
+                        </div>
+                        <div class="number">
+                            <span>+39 xxxxxxxxx</span>
+                        </div>
+                        <div class="email">
+                            <span>info@deliveroo.it</span>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Copy --}}
+                <div class="copy">
+                    <span>By Team Procas | Copyright &copy; 2022</span>
+                </div>
+
+            </div>
+        </footer>
     </div>
 </body>
 </html>
@@ -91,19 +149,67 @@
         left: 0;
         right: 0;
     }
-
     .nav-item {
         font-weight: 500;
         font-size: 17px;
     }
-
     .nav-link,  .navbar-brand{
         color: white !important;
     }
-
     .navbar-light .navbar-toggler {
         color: white;
         border-color: white;
         background-color: white
+    }
+    li {
+        list-style-type: none;
+    }
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    a:hover {
+        text-decoration: none;
+        color: inherit;
+    }
+    footer {
+        height: 400px;
+        background-color: #49BEB7;
+    }
+
+    .footer-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 100%
+    }
+    .footer-content h2{
+        font-size: 60px;
+        color: white;
+    }
+
+    .footer-content .copy {
+        display: flex;
+        justify-content: center;
+        color: white;
+    }
+    .links {
+        display: flex;
+        margin: 30px 0;
+    }
+
+    .links .title {
+        margin-bottom: 15px;
+        color: white;
+    }
+
+    .links span {
+        line-height: 40px;
+        color: white;
+    }
+
+    .links .social, 
+    .links .contacts {
+        width: 200px;
     }
 </style>
